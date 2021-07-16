37b8fa65c6fe10b9815db21521bb5fb1c771c5a5
140bpmdubstep <bpm140@fomalhaut.me>
2021-07-16 18:49:22 +0300

    Fixed drink commands

--- fs24bot3/ItemTraits/Drink.cs
+++ fs24bot3/ItemTraits/Drink.cs
@@ -22,11 +22,11 @@ namespace fs24bot3.ItemTraits
         public async Task<bool> OnUseMyself(Bot botCtx, string channel, Core.User user)
         {
             var rand = new Random();
-            var sms = botCtx.MessageBus.Where(x => x.Prefix.From == user.Username && !x.Trailing.StartsWith("@"));
+            var sms = botCtx.MessageBus.Where(x => x.Prefix.From == user.Username && !x.Trailing.StartsWith("@")).OrderBy(s => rand.Next(0, 2) == 1);
 
             if (sms.Any())
             {
-                string randStr = string.Join(" ", sms.First().Trailing.Split(" ").OrderBy(s => (rand.Next(DrunkLevel) % 2) == 0));
+                string randStr = string.Join(" ", sms.First().Trailing.Split(" ").OrderBy(s => (rand.Next(1, 5)) <= DrunkLevel));
                 string message = await Core.Transalator.TranslatePpc(randStr, "ru");
                 await botCtx.SendMessage(channel, $"{user.Username} выпил {Name} ({DrunkLevel}) и сказал: {message}");
             }
