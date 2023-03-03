7492eeeafb1b8a079d91c18f78a752909599fefa
140bpmdubstep <bpm140@fomalhaut.me>
2023-03-03 23:42:11 +0300

    Updated CI scipt

--- fs24bot3/build-linux.bash
+++ fs24bot3/build-linux.bash
@@ -1,4 +1,6 @@
 #/usr/bin/bash
+
+# This is script for CI usage only
 rm -rf releases/
 rm -rf bin/x64/Release
 
@@ -6,9 +8,4 @@ mkdir releases/
 mkdir releases/linux-x64-shared
 
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true --self-contained false
-
-cp -r bin/Release/net6.0/linux-x64/publish releases/linux-x64-shared/
-
-zip -r releases/linux-x64-shared.zip releases/linux-x64-shared
-# this a special-incremental deploy
-zip -9 -j releases/fs24bot3.zip releases/linux-x64-shared/publish/fs24bot3
+cp -r bin/Release/net6.0/linux-x64/publish/* releases/linux-x64-shared/
