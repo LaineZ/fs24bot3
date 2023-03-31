ea558123383ce2838f0c39511dc3149f945ba111
140bpmdubstep <bpm140@fomalhaut.me>
2023-03-31 22:22:17 +0300

    Fixed bridge client bot crash in check

--- fs24bot3/Checks/CheckAdmin.cs
+++ fs24bot3/Checks/CheckAdmin.cs
@@ -1,4 +1,5 @@
 ﻿using fs24bot3.Core;
+using fs24bot3.Models;
 using fs24bot3.QmmandsProcessors;
 using Qmmands;
 using System.Threading.Tasks;
@@ -10,9 +11,16 @@ public sealed class CheckAdmin : CheckAttribute
     {
         var context = _ as CommandProcessor.CustomCommandContext;
 
-        return context.User.GetUserInfo().Admin > 1 || ConfigurationProvider.Config.Backend == Models.Backend.Basic
+        try
+        {
+            return context.User.GetUserInfo().Admin > 1 || ConfigurationProvider.Config.Backend == Models.Backend.Basic
             ? CheckResult.Successful
-            : CheckResult.Failed("Это команда только для админов!");
+            : CheckResult.Failed("Эта команда только для админов!");
+        }
+        catch (Exceptions.UserNotFoundException)
+        {
+            return CheckResult.Failed("Эта команда только для пользователей fs24_bot и только для админов!");
+        }
     }
 
     public override string ToString()
