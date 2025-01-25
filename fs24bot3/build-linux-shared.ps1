5b205e96da014ad9a61be453bdb8d75058a5403d
140bpmdubstep <bpm140@fomalhaut.me>
2025-01-25 12:15:35 +0300

    Bump to .NET 7.0

--- fs24bot3/build-linux-shared.ps1
+++ fs24bot3/build-linux-shared.ps1
@@ -6,7 +6,7 @@ New-Item -ItemType Directory -Path ./releases\linux-x64-shared
 
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true --self-contained false
 
-Copy-Item -Recurse -Path .\bin\Release\net6.0\linux-x64\publish\* -Destination .\releases\linux-x64-shared
+Copy-Item -Recurse -Path .\bin\Release\net8.0\linux-x64\publish\* -Destination .\releases\linux-x64-shared
 Compress-Archive -Path .\releases\linux-x64-shared -DestinationPath releases\linux-x64-shared.zip
 Compress-Archive -Path .\releases\linux-x64-shared\fs24bot3 -DestinationPath releases\fs24bot3.zip
 Read-Host -Prompt "Press Enter to continue"
